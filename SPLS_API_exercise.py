5a30569a123d3f0bacea6dfc3900237148ba9d5d
oza <oza.s@husky.neu.edu>
2018-05-26 20:59:20 -0400

    Staples interview spark and python

---
+++ SPLS_API_exercise.py
@@ -0,0 +1,99 @@
+from flask import request
+from flask import jsonify, make_response
+from flask import Flask
+from shapely.geometry import Polygon, Point
+from pyspark import SparkContext, SQLContext
+
+
+# =====================================================
+
+def estimate_price(house_feat):
+    price = 0.0
+    price += 75.0 * house_feat['GrLivArea']
+    price += 54.0 * house_feat['TotalBsmtSF']
+    price += 51.0 * house_feat['GarageArea']
+    price += 671.0 * (house_feat['YearBuilt'] - 1990)
+
+    return price
+
+
+# =====================================================
+
+app = Flask(__name__)
+
+
+@app.route('/houseprice', methods=["GET"])
+def elements():
+    house_data = {}
+    try:
+        house_data['GrLivArea'] = int(request.args.get('GrLivArea'))
+        house_data['TotalBsmtSF'] = int(request.args.get('TotalBsmtSF'))
+        house_data['GarageArea'] = int(request.args.get('GarageArea'))
+        house_data['YearBuilt'] = int(request.args.get('YearBuilt'))
+    except Exception as e:
+        http_response = make_response(
+            jsonify({'Exception': 'All paramters must be numbers', 'ExceptionCause': e.args[0], 'ErrorCode': 500}))
+        return http_response
+
+    http_response = make_response(jsonify(
+        {'HouseFeatures': house_data, 'PriceEstimate': estimate_price(house_data)}
+    ))
+
+    return http_response
+
+
+@app.route('/houselookup', methods=["GET"])
+def getAllHousesUnderPolygon():
+
+    coordinateslist = []
+    i = 1
+    l = int(len(request.args)/2)
+    outputlist = []
+
+    for coord in range(int(len(request.args)/2)):
+        xc = "x" + str(i)
+        yc = "y" + str(i)
+        x = request.args.get(xc)
+        y = request.args.get(yc)
+        p = Point(float(x), float(y))
+        coordinateslist.append(p)
+        i += 1
+
+    poly = Polygon([[p.x, p.y] for p in coordinateslist])
+
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+
+    linedf = sqlContext.read.option("header","false").format("csv").load("/home/oza/Downloads/house_coordinates.csv")
+    rowdf = linedf.select("*").collect()
+    linedf.show()
+
+    for column in rowdf:
+        x1 = column[1]
+        y1 = column[2]
+        x2 = column[3]
+        y2 = column[4]
+        x3 = column[5]
+        y3 = column[6]
+        x4 = column[7]
+        y4 = column[8]
+
+        p = Point(float(x1), float(y1))
+
+        # We will check whether a given point is within polygon coordinates,
+        # If yes than we will add the house id inside the list
+        
+        if (isPointWithinPolygon(poly, p) == True):
+            outputlist.insert(column[0])
+
+    http_response = make_response(jsonify(outputlist))
+    return http_response
+
+def isPointWithinPolygon(polygon, point):
+    point.within(polygon)
+
+
+# =====================================================
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
